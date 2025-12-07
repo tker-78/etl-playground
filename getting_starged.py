a6e3aa3fdcea4dedb8e7ff5f55b640690d8634b1
takuya kinoshita <kktak02@gmail.com>
2025-12-07 14:31:49 +0900

    added getting-started

---
+++ getting_starged.py
@@ -0,0 +1,20 @@
+from prefect import flow, task
+import random
+
+@task
+def get_customer_ids() -> list[str]:
+    return [f"customer{n}" for n in random.choices(range(100), k=10)]
+
+@task
+def process_customer(customer_id: str) -> str:
+    return f"Processed {customer_id}"
+
+@flow
+def main() -> list[str]:
+    customer_ids = get_customer_ids()
+    results = process_customer.map(customer_ids)
+    return results
+
+if __name__ == "__main__":
+    main()
+
