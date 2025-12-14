4d94f9c65dfdf99609dbf77155d8ff1789232cd3
takuya kinoshita <kktak02@gmail.com>
2025-12-15 06:59:47 +0900

    added dbt

--- _types.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from pydantic import BaseModel
-
-
-class JobRequest(BaseModel):
-    job_id: str
-    job_type: str
-    payload: dict[str, Any]
--- main.py
+++ main.py
@@ -2,30 +2,30 @@ import uvicorn
 from cachetools import TTLCache
 from fastapi import FastAPI
 
-from _types import JobRequest
-from tasks import process_job
+# from _types import JobRequest
+# from tasks import process_job
 
 app = FastAPI()
 
-job_cache: TTLCache = TTLCache(maxsize=100, ttl=60)
+# job_cache: TTLCache = TTLCache(maxsize=100, ttl=60)
 
 
-@app.post("/job")
-def submit_job(job_request: JobRequest):
-    future = process_job.delay(job_request)
-    job_cache[job_request.job_id] = future
-    return {"message": "Job submitted"}
-
-
-@app.get("/job/{job_id}")
-def read_job(job_id: str):
-    return job_cache[job_id]
-
-
-@app.get("/jobs")
-def read_jobs():
-    return list(job_cache.keys())
-
+# @app.post("/job")
+# def submit_job(job_request: JobRequest):
+#     future = process_job.delay(job_request)
+#     job_cache[job_request.job_id] = future
+#     return {"message": "Job submitted"}
+#
+#
+# @app.get("/job/{job_id}")
+# def read_job(job_id: str):
+#     return job_cache[job_id]
+#
+#
+# @app.get("/jobs")
+# def read_jobs():
+#     return list(job_cache.keys())
+#
 
 @app.get("/health")
 def health():
